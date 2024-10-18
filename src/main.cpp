a974dce200e6739b5639ad4d878ac229e09e269d
qq11123334 <qq11123334@csie.io>
2024-10-18 09:26:26 +0800

    fixed path prob bug

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,6 @@ int main(){
             double A = 0.25, B = 0.75, tao = default_setting["tao"], T = 10, n = 2;
             string parameter = to_string(num_nodes) + " " + to_string(entangle_lambda) + " " + to_string(tao) + " " + to_string(entangle_time);
             cerr << (command + filename + " " + parameter) << endl;
-            assert(false);
             if(system((command + filename + " " + parameter).c_str()) != 0){
                 cerr<<"error:\tsystem proccess python error"<<endl;
                 exit(1);
