c39d3d187f8f9024194ad374d9d54e5375b23ac9
qq11123334 <qq11123334@csie.io>
2024-10-25 13:22:25 +0800

    update shape operator

--- src/Algorithm/MyAlgo1/MyAlgo1.cpp
+++ src/Algorithm/MyAlgo1/MyAlgo1.cpp
@@ -77,7 +77,7 @@ pair<Shape_vector, double> MyAlgo1::find_min_shape(int src, int dst, double alp)
         int best_time = -1;
         for(int t = 0; t < time_limit; t++) {
             double result = recursion_calculate_min_shape(0, path.size() - 1, t, path);
-            result = (result + alp);
+            result = (result + alp) / graph.path_Pr(path);
             if(best > result) {
                 best = result;
                 best_time = t;
